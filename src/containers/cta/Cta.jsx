3ceae6273523c278e72dba5ee2c09934725353cd
psthely <contact@philippesthely.com>
2023-02-23 22:31:49 +0100

    add Cta component

--- src/containers/cta/Cta.jsx
+++ src/containers/cta/Cta.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './cta.css'
 
+import substract from '../../assets/substract.png'
+
 
 const Cta = () => {
     return (
-        <div>Cta</div>
+        <div className='cta'>
+            <img src={ substract } alt='substract' />
+            <div className='cta__content'>
+                <h3>Subscribe to my blog.</h3>
+                <p>I post fresh content every week.</p>
+                <div className='cta__content__form'>
+                    <input type='text' placeholder='Email adress' />
+                    <button>SUBSCRIBE</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
